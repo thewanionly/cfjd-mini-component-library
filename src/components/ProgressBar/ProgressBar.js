5ae97c17592f2c492746c4a64692f8bc28fa6661
thewanionly <pelloani@gmail.com>
2025-01-21 03:29:19 +0800

    exercise-1: add size prop

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
@@ -8,12 +8,40 @@ import VisuallyHidden from '../VisuallyHidden';
 const MIN_VALUE = 0;
 const MAX_VALUE = 100;
 
+const SIZES = {
+  small: {
+    bar: css`
+      border-radius: 4px;
+    `,
+    value: css`
+      height: 8px;
+    `
+  },
+  medium: {
+    bar: css`
+      border-radius: 4px;
+    `,
+    value: css`
+      height: 12px;
+    `
+  },
+  large: {
+    bar: css`
+      padding: 4px;
+      border-radius: 8px;
+    `,
+    value: css`
+      height: 16px;
+    `
+  }
+};
+
 const S = {
   ProgressBar: styled.div`
-    padding: 4px;
     background-color: ${COLORS.transparentGray15};
-    border-radius: 8px;
     box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+    ${({ $size }) => SIZES[$size].bar};
   `,
   ProgressValue: styled.div`
     background-color: ${COLORS.primary};
@@ -22,14 +50,15 @@ const S = {
     border-top-right-radius: ${({ $value }) => $value >= MAX_VALUE && '4px'};
     border-bottom-right-radius: ${({ $value }) => $value >= MAX_VALUE && '4px'};
     width: ${({ $value }) => `${Math.min(Math.max($value, MIN_VALUE), MAX_VALUE)}%`};
-    height: 16px;
+
+    ${({ $size }) => SIZES[$size].value};
   `
 };
 
-const ProgressBar = ({ value, size }) => {
+const ProgressBar = ({ value, size = 'medium' }) => {
   return (
-    <S.ProgressBar role='progressbar' aria-valuenow={value}>
-      <S.ProgressValue $value={value} />
+    <S.ProgressBar role='progressbar' aria-valuenow={value} $size={size}>
+      <S.ProgressValue $value={value} $size={size} />
     </S.ProgressBar>
   );
 };
