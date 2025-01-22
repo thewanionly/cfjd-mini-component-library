cd5753edeafb41deee06014ca210c8125757e51f
thewanionly <pelloani@gmail.com>
2025-01-23 05:03:51 +0800

    execise-2: initial styling

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,27 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const S = {
+  Select: styled.select`
+    background-color: ${COLORS.transparentGray15};
+    border: none;
+    border-radius: 8px;
+    padding: 12px 16px;
+    height: 43px;
+
+    color: ${COLORS.gray700};
+    font-size: 1rem;
+    font-weight: 400;
+  `
+};
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <S.Select value={value} onChange={onChange}>
       {children}
-    </select>
+    </S.Select>
   );
 };
 
